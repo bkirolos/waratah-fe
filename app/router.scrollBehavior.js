274b259761a671b0bc5473d046b64cb140233603
bkirolos <hey@bkirolos.com>
2022-02-03 14:27:46 -0800

    add smooth scroll

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,14 @@
+export default (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition
+  }
+
+  if (to.hash) {
+    return {
+      selector: to.hash,
+      behavior: 'smooth'
+    }
+  }
+
+  return { x: 0, y: 0, behavior: 'smooth' }
+}
