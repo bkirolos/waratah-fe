2987428dc1ed6356db419829a333f1d98479db4f
bkirolos <hey@bkirolos.com>
2022-02-10 13:57:52 -0800

    add image description

--- _sanity/schemas/objects/hero.js
+++ _sanity/schemas/objects/hero.js
@@ -44,6 +44,7 @@ export default {
       type: 'imagePlus',
       name: 'image',
       title: 'Image',
+      description: 'Image should have a square aspect ratio',
       validation: Rule => Rule.required().error('Image is required')
     },
     {
