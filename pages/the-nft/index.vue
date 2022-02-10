b016e3b0142ad2d09aea9003fce34c8a34427843
bkirolos <hey@bkirolos.com>
2022-02-10 14:51:26 -0800

    slug layout

--- pages/the-nft/index.vue
+++ pages/the-nft/index.vue
@@ -1,16 +1,42 @@
 <template>
-  <section class="grid grid-cols-12 px-4 py-20">
-    <h1 class="heading-2 md:col-start-2">The NFT's</h1>
-    <nuxt-link to="/the-nft/1221">Go</nuxt-link>
+  <section class="grid w-full grid-cols-12 px-4 py-20">
+    <h1 class="heading-2 md:col-start-2 col-span-10">The NFT's</h1>
+    <div class="md:col-start-2 col-span-10 grid nft-asset-grid">
+      <div
+        v-for="item in nftList"
+        :key="item.tokenId.current"
+        class="nft-grid-item"
+      >
+        <LazyImage v-if="item.image" class="w-full" :image="item.image" />
+        <h5>Flying Formation #{{ item.tokenId.current }}</h5>
+        <p>Shoe Size {{ item.shoeSize }}</p>
+        <nuxt-link :to="`/the-nft/${item.slug.current}`">
+          View Details
+        </nuxt-link>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import allNfts from '@/groq/allNfts'
+
 export default {
   data() {
     return {
-      page: null
+      nftList: null
     }
+  },
+  async fetch() {
+    const data = await this.$sanity.fetch(allNfts)
+    this.nftList = data
   }
 }
 </script>
+<style lang="scss">
+.nft-asset-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+</style>
