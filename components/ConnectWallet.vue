e6d04359ed85469cb3c5a41a5c2e00e0743c696e
bkirolos <hey@bkirolos.com>
2022-02-17 15:40:48 -0800

    update connect wallet button states

--- components/ConnectWallet.vue
+++ components/ConnectWallet.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="flex">
-    <button class="hover-transition p-2" @click="$web3.connectWallet()">
-      <span class="cta flex items-center h-10 leading-none px-5 py-0">
+    <button class="p-2" @click="handleClick">
+      <span
+        :class="[
+          'connect-button cta flex items-center h-10 leading-none px-5 py-0 relative',
+          { 'bg-salmon transition-colors': isConnected && isWrongNetwork }
+        ]"
+      >
         {{ connectText }}
-      </span>
-    </button>
-    <button
-      v-if="isConnected"
-      class="hover-transition p-2"
-      @click="$web3.clearConnection()"
-    >
-      <span class="cta flex items-center h-10 leading-none px-5 py-0">
-        Disconnect
+        <span
+          v-if="isConnected && isWrongNetwork"
+          class="connect-button-helper left-0 pt-2 absolute right-0"
+        >
+          Connect to Mainnet
+        </span>
       </span>
     </button>
   </div>
@@ -20,16 +22,43 @@
 <script>
 export default {
   computed: {
+    connectText() {
+      return this.isConnected
+        ? this.isWrongNetwork
+          ? 'Wrong Network'
+          : 'Disconnect'
+        : 'Connect Wallet'
+    },
     isConnected() {
       return this.$web3?.accounts
     },
-    connectText() {
-      return this.isConnected
-        ? `Connected to ${this.$web3?.network?.name}`
-        : 'Connect Wallet'
+    isWrongNetwork() {
+      if (this.network === 'mainnet') {
+        return this.network !== 'homestead'
+      } else {
+        return this.netwrok !== this.$config.ethereumNetwork
+      }
+    },
+    network() {
+      return this.$web3?.network?.name
+    }
+  },
+  methods: {
+    handleClick() {
+      this.isConnected
+        ? this.$web3.clearConnection()
+        : this.$web3.connectWallet()
     }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.connect-button {
+  min-width: 176px;
+  &-helper {
+    bottom: -1rem;
+    font-size: 10px;
+  }
+}
+</style>
