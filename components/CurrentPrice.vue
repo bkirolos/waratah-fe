c4de52e2380135335ee2cf5452e9d0f0c83f167a
bkirolos <hey@bkirolos.com>
2022-02-16 19:09:54 -0800

    touch up current price and index layout

--- components/CurrentPrice.vue
+++ components/CurrentPrice.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h2 class="body font-bold mb-4">Current Price</h2>
-    <p class="flex space-x-4">
+    <p class="flex items-center space-x-4">
       <span class="heading-3 leading-none">{{ priceETH }} ETH</span>
-      <span v-if="priceUSD" class="body font-bold mt-3">({{ priceUSD }})</span>
+      <span v-if="priceUSD" class="body font-bold -mt-3">({{ priceUSD }})</span>
     </p>
   </div>
 </template>
