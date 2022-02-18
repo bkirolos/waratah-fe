2beee1441aef39d02fdd73626010fe356a0c7f7f
bkirolos <hey@bkirolos.com>
2022-02-18 11:54:03 -0800

    update sanity client config for use with preview iframe

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,8 @@ export default {
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: process.env.SANITY_DATASET,
     apiVersion: process.env.SANITY_API_VERSION,
+    withCredentials: true,
+    useCdn: false,
     minimal: true
   },
 
