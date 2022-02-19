f2559ce420f9bdd3230ea3dd9c2c237b0bf3e653
bkirolos <hey@bkirolos.com>
2022-02-18 17:16:10 -0800

    qa width of nft video asset

--- pages/flyingformations/_slug.vue
+++ pages/flyingformations/_slug.vue
@@ -189,24 +189,18 @@ export default {
 </script>
 
 <style lang="scss">
-$slide-width: clamp(1px, 100%, 670px);
 .nft-video-asset,
 .shoe-asset {
-  flex-shrink: 0;
   height: 0;
-  margin-right: 0.5rem;
-  padding-bottom: $slide-width;
+  padding-bottom: 100%;
   position: relative;
-  width: $slide-width;
+  width: 100%;
   img,
   .plyr {
-    position: absolute;
-    width: 100%;
     height: 100%;
     object-fit: cover;
+    position: absolute;
+    width: 100%;
   }
-}
-.shoe-asset {
-  width: clamp(1px, 100%, 660px);
 }
 </style>
