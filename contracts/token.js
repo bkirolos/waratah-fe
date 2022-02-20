c79955742e1f6cc9ecb84fbf5235ff65680b9e8e
bkirolos <hey@bkirolos.com>
2022-02-19 23:53:42 -0800

    mainnet contract

--- contracts/token.js
+++ contracts/token.js
@@ -1,13 +1,16 @@
 import TokenRinkeby from "./deployments/rinkeby/FlyingFormations.json";
 import TokenLocal from "./deployments/localhost/FlyingFormations.json";
+import TokenMainnet from "./deployments/mainnet/FlyingFormations.json";
 
 export const Token = {
   address: {
     rinkeby: TokenRinkeby.address,
     localhost: TokenLocal.address,
+    homestead: TokenMainnet.address,
   },
   abi: {
     rinkeby: TokenRinkeby.abi,
     localhost: TokenLocal.abi,
+    homestead: TokenMainnet.abi,
   },
 };
