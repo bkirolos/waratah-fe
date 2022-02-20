e0b705b721274e6ff8ea5fd67f0e72fac8ab723b
bkirolos <hey@bkirolos.com>
2022-02-20 12:22:35 -0800

    update text

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -53,7 +53,7 @@ export default {
       return this.currentTime >= this.startingTime
     },
     displayEndTime() {
-      return "February 20nd at 10:00PM PST"
+      return "February 20th at 10:00PM PST"
       // return this.endingTime.format('MMMM Do [at] h:mm A z')
     },
     displayStartTime() {
