3dd0157d82cc7d2c08920bee9d84c786474b8d2a
bkirolos <hey@bkirolos.com>
2022-05-04 17:49:41 -0500

    basic styling

---
+++ pages/flyingformations/redeem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col w-full">
+    <AssetRedemptionModal
+      :isRedeemable="isRedeemable"
+      :startRedemption="startRedemption"
+    />
+  </div>
+</template>
+
+<script>
+import head from '@/mixins/head'
+
+export default {
+  mixins: [head],
+  data() {
+    return {
+      isRedeemable: true
+    }
+  },
+
+  methods: {
+    startRedemption() {
+      this.isRedeemable = false
+    }
+  }
+}
+</script>
