860a422295b4ec98ca2085c04213910ac9f1b200
bkirolos <hey@bkirolos.com>
2022-05-06 14:03:07 -0500

    fetch settings

--- components/AssetRedemptionModal.vue
+++ components/AssetRedemptionModal.vue
@@ -1,11 +1,20 @@
 <template>
-  <TwoUp v-if="showTwoUpModal" :cta="setModalClosed" />
+  <TwoUp v-if="showTwoUpModal" :cta="setModalClosed" :page="page" />
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import dropSettings from '@/groq/dropSettings'
 
 export default {
+  data() {
+    return {
+      page: null
+    }
+  },
+  async fetch() {
+    this.page = await this.$sanity.fetch(dropSettings)
+  },
   computed: {
     ...mapGetters({
       showTwoUpModal: 'ui/showTwoUpModal'
