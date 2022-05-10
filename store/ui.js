22e5591fc0e3b19a27ff6709c9bc024339ab601f
bkirolos <hey@bkirolos.com>
2022-05-10 12:35:03 -0700

    remove unused store variables

--- store/ui.js
+++ store/ui.js
@@ -1,27 +1,19 @@
 export const state = () => ({
-  showRedemptionModal: true,
-  redemptionSettings: null
+  showRedemptionModal: true
 })
 
 export const mutations = {
   SET_REDEMPTION_MODAL_CLOSED(state) {
     state.showRedemptionModal = false
-  },
-  SET_REDEMPTION_SETTINGS(state, settings) {
-    state.redemptionSettings = settings
   }
 }
 
 export const actions = {
   setRedemptionModalClosed({ commit }) {
     commit('SET_REDEMPTION_MODAL_CLOSED')
-  },
-  setRedemptionSettings({ commit }, settings) {
-    commit('SET_REDEMPTION_SETTINGS', settings)
   }
 }
 
 export const getters = {
-  showRedemptionModal: state => state.showRedemptionModal,
-  redemptionSettings: state => state.redemptionSettings
+  showRedemptionModal: state => state.showRedemptionModal
 }
