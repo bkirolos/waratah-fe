4b9dab9184277fed48d223c7bf684a80644af58a
bkirolos <hey@bkirolos.com>
2022-05-15 12:32:28 -0700

    rm console

--- plugins/web-3.js
+++ plugins/web-3.js
@@ -202,9 +202,7 @@ export default ({ $config: { infuraId, ethereumNetwork } }, inject) => {
       // Force update on OpenSea
       try {
         const url = `https://api.opensea.io/api/v1/asset/0x7ADBa40714BFFDc3c8d35f36971DfF9C8FA24E15/${this.tokenId}?force_update=true`
-        const req = await this.$axios.$get(url)
-
-        console.log(req)
+        await this.$axios.$get(url)
       } catch (e) {
         console.error(e)
       }
